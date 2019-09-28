82b2c2310641653216f36da832f628b1cdbd2453
SterlingCorey <logan@coldiron.ca>
2019-09-28 13:40:54 -0400

    added heroku dependencies

--- flip_job_search/settings.py
+++ flip_job_search/settings.py
@@ -128,5 +128,6 @@ CSRF_COOKIE_NAME = 'csrftoken'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
