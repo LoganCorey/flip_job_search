1ae40f0bbee9ba990acefd42e55e3d76243e25b7
logan.corey <logan.corey@mail.utoronto.ca>
2019-09-29 23:27:07 -0400

    removed django setuo

--- flip_job_search/settings.py
+++ flip_job_search/settings.py
@@ -14,8 +14,7 @@ import os
 import dj_database_url
 import django_heroku
 import dotenv
-import django
-django.setup()
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
