0bcd977ef8f4e6bdf2c57de726bc092536f8fcda
SterlingCorey <logan@coldiron.ca>
2019-10-01 00:28:49 -0400

    removed dummy dictionary

--- flip/utils/json_creator/creator.py
+++ flip/utils/json_creator/creator.py
@@ -14,22 +14,12 @@ def createJson(job_text: str, resume_text: str):
     job_skills = flash_match(job_text, job_text_frequencies)
     resume_skills = flash_match(resume_text, resume_text_frequencies)
     match_rate = int(job_skills.compare(resume_skills) * 100)
+    
     skills_dictionary = {
         "job_skills": job_skills.skills,
         "resume_skills": resume_skills.skills,
         "match_rate": match_rate
     }
-    """
-    skills_dictionary = {
-        "job_skills":{
-            "logan":1
-        },
-        'resume_skills':{
-        "logan":1
-        },
-        'match_rate':100
-    }
-    """
 
     return skills_dictionary
 
