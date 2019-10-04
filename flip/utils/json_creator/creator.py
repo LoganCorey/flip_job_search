5452d79a0e2635a0f7b113d0c6ea8570e8e4faea
SterlingCorey <logan@coldiron.ca>
2019-10-04 16:01:02 -0400

    reverting os path changes

--- flip/utils/json_creator/creator.py
+++ flip/utils/json_creator/creator.py
@@ -1,6 +1,5 @@
 from flip.utils.processor.basic_process import BasicProcessing
 from flip.utils.matcher.skill_matcher import  SkillSet, flash_match
-from pathlib import Path
 import os
 
 
@@ -15,8 +14,8 @@ def createJson(job_text: str, resume_text: str):
     processor = BasicProcessing()
     job_text_frequencies = processor.process(job_text)
     resume_text_frequencies = processor.process(resume_text)
-    job_skills = flash_match(job_text, job_text_frequencies, filename=os.path.abspath("all_linked_skills.txt"))
-    resume_skills = flash_match(resume_text, resume_text_frequencies, filename=os.path.abspath("all_linked_skills.txt"))
+    job_skills = flash_match(job_text, job_text_frequencies)
+    resume_skills = flash_match(resume_text, resume_text_frequencies)
     match_rate = int(job_skills.compare(resume_skills) * 100)
 
     skills_dictionary = {
